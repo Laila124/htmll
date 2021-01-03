2a386a709926dac965b4376b3b2893f7e01e0c70
laila124 <llqais1995@gmail.com>
2021-01-03 11:37:46 +0200

    class06

---
+++ java.js
@@ -0,0 +1,23 @@
+var number = 15; 
+var text = 'pepsi';
+var correct = true;
+var notcorrect = false;
+// alert(number);
+//alert(text);
+//alert(correct);
+//alert(notcorrect
+
+if ("text == pepsi"){
+    alert ("text is pepsi");
+}else {
+    alert ("text is not pepsi");
+}
+    if (correct){
+        alert(true)
+    }
+if (notcorrect){
+    alert (false)
+}
+if (number != 10){
+    alert (number)
+}
